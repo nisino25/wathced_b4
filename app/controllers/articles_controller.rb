415d9808ec1c71e26ebf7b4ff2ba4207f2ce5bd9
nisino25 <nisino25@gmail.com>
2020-06-05 14:25:23 -0700

    Now showing the categories on shoew and index of article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update, :destroy]
   
     def show
+      article = @article.category_ids
     end
   
     def index
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
     validates :title, presence: true, length: { minimum: 3, maximum: 100 }
     has_many :article_categories
-    has_many :category, through: :article_categories
+    has_many :categories, through: :article_categories
 end
