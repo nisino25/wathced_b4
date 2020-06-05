0a4a6c328d7bf74c207a53b770b16a4910432467
nisino25 <nisino25@gmail.com>
2020-06-05 00:01:26 -0700

    Added the grid so the articles look good now

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -8,7 +8,7 @@ class SessionController < ApplicationController
       if user && user.authenticate(params[:session][:password])
         session[:user_id] = user.id
         flash[:notice] = "Logged in successfully"
-        redirect_to root_path
+        redirect_to articles_path
       else
         flash.now[:alert] = "There was something wrong with your login details"
         render 'new'
